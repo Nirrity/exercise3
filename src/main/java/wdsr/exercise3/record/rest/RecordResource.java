08d6af95d5967469044cb17d1777547749fbb63e
Nirrity <martawijatkowska.95@gmail.com>
2017-06-06 00:26:04 +0200

    Created HTTP metods: GET, PUT, POST, DELETE

--- src/main/java/wdsr/exercise3/record/rest/RecordResource.java
+++ src/main/java/wdsr/exercise3/record/rest/RecordResource.java
@@ -1,7 +1,21 @@
 package wdsr.exercise3.record.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import wdsr.exercise3.record.Record;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,46 +29,58 @@ public class RecordResource {
 	@Inject
 	private RecordInventory recordInventory;
 	
-	/**
-	 * TODO Add methods to this class that will implement the REST API described below.
-	 * Use methods on recordInventory to create/read/update/delete records. 
-	 * RecordInventory instance (a CDI bean) will be injected at runtime.
-	 * Content-Type used throughout this exercise is application/xml.
-	 * API invocations that must be supported:
-	 */
+	@GET
+	@Produces("application/xml")
+	public Response getAllRecords() {
+		List<Record> result = new ArrayList<>();
+		result.addAll(recordInventory.getRecords());
+		GenericEntity<List<Record>> genericEntity = new GenericEntity<List<Record>>(result) {};
+		return Response.ok(genericEntity).build();
+	}
+	@POST
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	public Response addNewRecord(Record record) 
+	{
+		if(record.getId() != null)
+			return Response.status(Status.BAD_REQUEST).build();
+		this.recordInventory.addRecord(record);
+		return Response.status(Status.CREATED).entity(record).build();
+	}
 	
-	/**
-	 * * GET https://localhost:8091/records
-	 * ** Returns a list of all records (as application/xml)
-	 * ** Response status: HTTP 200
-	 */
+	@GET
+	@Path("/{id}")
+	@Produces("application/xml")
+	public Response getRecordById(@PathParam(value = "id") int id) {
+		Record record = recordInventory.getRecord(id);
+		if(record != null)
+			return Response.ok(record).build();
+		else 
+			return Response.status(Status.NOT_FOUND).build();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	public Response updateRecordById(Record record, @PathParam(value = "id") int id){
+		if (record.getId() != null && id != record.getId() )
+			return Response.status(Status.BAD_REQUEST).build();
 
-	/**
-	 * POST https://localhost:8091/records
-	 * ** Creates a new record, returns ID of the new record.
-	 * ** Consumes: record (as application/xml), ID must be null.
-	 * ** Response status if ok: HTTP 201, Location header points to the newly created resource.
-	 * ** Response status if submitted record has ID set: HTTP 400
-	 */
+		if (this.recordInventory.updateRecord(id, record)) 
+			return Response.status(Status.NO_CONTENT).build();
+		else 
+			return Response.status(Status.NOT_FOUND).build();
+	}
 	
-	/**
-	 * * GET https://localhost:8091/records/{id}
-	 * ** Returns an existing record (as application/xml)
-	 * ** Response status if ok: HTTP 200
-	 * ** Response status if {id} is not known: HTTP 404
-	 */
-	 /**
-	 * * PUT https://localhost:8091/records/{id}
-	 * ** Replaces an existing record in entirety.
-	 * ** Submitted record (as application/xml) must have null ID or ID must be identical to {id} in the path.
-	 * ** Response status if ok: HTTP 204
-	 * ** Response status if {id} is not known: HTTP 404
-	 */
-	
-	/**
-	 * * DELETE https://localhost:8091/records/{id}
-	 * ** Deletes an existing record.
-	 * ** Response status if ok: HTTP 204
-	 * ** Response status if {id} is not known: HTTP 404
-	 */
+	@DELETE
+	@Path("/{id}")
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	public Response deleteRecordById(@PathParam(value = "id") int id) {
+		if(recordInventory.deleteRecord(id))
+			return Response.status(Status.NO_CONTENT).build();
+		else
+			return Response.status(Status.NOT_FOUND).build();
+	}
 }
